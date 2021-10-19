35fee7f9ff4584b13ec29c558b685edeba648711
vaishnavi2906 <vaishnavi2906@gmail.com>
2021-10-19 21:38:55 +0530

    all test cases

--- foobarqix.go
+++ foobarqix.go
@@ -13,38 +13,46 @@ func Compute(input string) string {
 	s := ""
 	if i%3 == 0 {
 		s = s + "Foo"
-	} else if i%5 == 0 {
+	}
+	if i%5 == 0 {
 		s = s + "Bar"
-	} else if i%7 == 0 {
+	}
+	if i%7 == 0 {
 		s = s + "Qix"
-	} else {
+	}
+
+	for j := 0; j < len(input); j++ {
+		if input[j] == '3' {
+			s = s + "Foo"
+		} else if input[j] == '5' {
+			s = s + "Bar"
+		} else if input[j] == '7' {
+			s = s + "Qix"
+		}
+	}
+
+	if len(s) == 0 {
 		return input
 	}
-
-	if i == 3 {
-		s = s + "Foo"
-	} else if i == 5 {
-		s = s + "Bar"
-	} else if i == 7 {
-		s = s + "Qix"
-	}
-
 	return s
-
-	// if input == "3" {
-	// 	return "Foo"
-	// }
-
-	// if input == "5" {
-	// 	return "Bar"
-	// }
-
-	// if input == "7" {
-	// 	return "Qix"
-	// } else {
-	// 	return input
-	// }
-
-	// return "0"
-	// return "1"
 }
+
+//return s
+
+// if input == "3" {
+// 	return "Foo"
+// }
+
+// if input == "5" {
+// 	return "Bar"
+// }
+
+// if input == "7" {
+// 	return "Qix"
+// } else {
+// 	return input
+// }
+
+// return "0"
+// return "1"
+//}
--- foobarqix_test.go
+++ foobarqix_test.go
@@ -12,7 +12,7 @@ func TestCompute_ShouldReturn1_GivenTheInputIs1(t *testing.T) {
 	}
 }
 
-func TestCompute_ShouldReturn2_GivenTheInputIs1(t *testing.T) {
+func TestCompute_ShouldReturn2_GivenTheInputIs2(t *testing.T) {
 	input := "2"
 	result := Compute(input)
 
@@ -21,7 +21,7 @@ func TestCompute_ShouldReturn2_GivenTheInputIs1(t *testing.T) {
 	}
 }
 
-func TestCompute_ShouldReturnfoo_GivenTheInputIs3(t *testing.T) {
+func TestCompute_ShouldReturnfoofoo_GivenTheInputIs3(t *testing.T) {
 	input := "3"
 	result := Compute(input)
 
@@ -30,7 +30,7 @@ func TestCompute_ShouldReturnfoo_GivenTheInputIs3(t *testing.T) {
 	}
 }
 
-func TestCompute_ShouldReturn4_GivenTheInputIs1(t *testing.T) {
+func TestCompute_ShouldReturn4_GivenTheInputIs4(t *testing.T) {
 	input := "4"
 	result := Compute(input)
 
@@ -39,7 +39,7 @@ func TestCompute_ShouldReturn4_GivenTheInputIs1(t *testing.T) {
 	}
 }
 
-func TestCompute_ShouldReturnBar_GivenTheInputIs5(t *testing.T) {
+func TestCompute_ShouldReturnBarBar_GivenTheInputIs5(t *testing.T) {
 	input := "5"
 	result := Compute(input)
 
@@ -48,12 +48,12 @@ func TestCompute_ShouldReturnBar_GivenTheInputIs5(t *testing.T) {
 	}
 }
 
-func TestCompute_ShouldReturnQix_GivenTheInputIs7(t *testing.T) {
+func TestCompute_ShouldReturnQixQix_GivenTheInputIs7(t *testing.T) {
 	input := "7"
 	result := Compute(input)
 
 	if result != "QixQix" {
-		t.Errorf("Expected %v but got %v", "Qix", result)
+		t.Errorf("Expected %v but got %v", "QixQix", result)
 	}
 }
 
@@ -63,5 +63,86 @@ func TestCompute_ShouldReturnfoo_GivenTheInputIs6(t *testing.T) {
 
 	if result != "Foo" {
 		t.Errorf("Expected %v but got %v", "Foo", result)
+	}
+}
+
+func TestCompute_ShouldReturn8_GivenTheInputIs8(t *testing.T) {
+	input := "8"
+	result := Compute(input)
+
+	if result != "8" {
+		t.Errorf("Expected %v but got %v", "8", result)
+	}
+}
+
+func TestCompute_ShouldReturnfoo_GivenTheInputIs9(t *testing.T) {
+	input := "9"
+	result := Compute(input)
+
+	if result != "Foo" {
+		t.Errorf("Expected %v but got %v", "Foo", result)
+	}
+}
+
+func TestCompute_ShouldReturn10_GivenTheInputIs10(t *testing.T) {
+	input := "10"
+	result := Compute(input)
+
+	if result != "Bar" {
+		t.Errorf("Expected %v but got %v", "Bar", result)
+	}
+}
+
+func TestCompute_ShouldReturn13_GivenTheInputIs13(t *testing.T) {
+	input := "13"
+	result := Compute(input)
+
+	if result != "Foo" {
+		t.Errorf("Expected %v but got %v", "Foo", result)
+	}
+}
+
+func TestCompute_ShouldReturnfoobarbar_GivenTheInputIs15(t *testing.T) {
+	input := "15"
+	result := Compute(input)
+
+	if result != "FooBarBar" {
+		t.Errorf("Expected %v but got %v", "FooBarBar", result)
+	}
+}
+
+func TestCompute_ShouldReturnfooqix_GivenTheInputIs21(t *testing.T) {
+	input := "21"
+	result := Compute(input)
+
+	if result != "FooQix" {
+		t.Errorf("Expected %v but got %v", "FooQix", result)
+	}
+}
+
+func TestCompute_ShouldReturnfoofoofoo_GivenTheInputIs33(t *testing.T) {
+	input := "33"
+	result := Compute(input)
+
+	if result != "FooFooFoo" {
+		t.Errorf("Expected %v but got %v", "FooFooFoo", result)
+	}
+}
+
+func TestCompute_ShouldReturnfoobar_GivenTheInputIs51(t *testing.T) {
+	input := "51"
+	result := Compute(input)
+
+	if result != "FooBar" {
+		t.Errorf("Expected %v but got %v", "FooBar", result)
+	}
+}
+
+func TestCompute_ShouldReturnbarfoo_GivenTheInputIs53(t *testing.T) {
+	input := "53"
+	result := Compute(input)
+
+	if result != "BarFoo" {
+		t.Errorf("Expected %v but got %v", "BarFoo", result)
 	}
 }
